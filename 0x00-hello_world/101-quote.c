0562de1cdf533b88e5cf8ea2a79b6ad7db44492a
ann developer <annwaiyegok@gmail.com>
2022-06-02 12:44:48 -0700

    101 quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+{
+#include <stdio.h>
+/**
+ * main - prints a string
+ *
+ * Description: prints a string
+ *
+ * Return: 1
+ *
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n");
+return (1);
+}
+}
