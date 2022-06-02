99a6f0aa4db619ef3328e457bf451fc9165e286f
ann developer <annwaiyegok@gmail.com>
2022-06-02 11:44:37 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
-
-
-
 /**
+ * main -prints a string
  *
- *  * main - Entry point
+ * Description: prints a string
  *
- *   *
+ *Return: 0
  *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
