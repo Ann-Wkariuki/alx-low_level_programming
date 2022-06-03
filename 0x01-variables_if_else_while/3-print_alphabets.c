dc9e96f1a0c24f1ee25ca6c3427495b8250e2a60
ann developer <annwaiyegok@gmail.com>
2022-06-03 07:11:33 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <atdlib.h>
+#include <time.h>
+/**
+ * main - prints alphabets in lowercase and uppercase
+ *
+ * Description: prints alphabets in lower nd upper case
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+	for (low = 'A'; low <= 'Z'; low++)
+		putchar(low);
+	putchar("\n");
+	return (0);
+}
