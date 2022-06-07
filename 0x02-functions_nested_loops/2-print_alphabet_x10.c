609be263c8df35794c23e36b6ddf90ca22347345
ann developer <annwaiyegok@gmail.com>
2022-06-07 05:41:23 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet)x10 - - prints lowercase alphabets 10 times
+ *
+ * Description: prints alphabets ten times
+ *
+ * Return: 0
+ *
+ */
+void print_alphabet_x10(void);
+{
+	int i;
+	char ann;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (ann = 'a'; ann <= 'z'; ann++)
+			putchar(ann);
+		{
+			putchar('\n');
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 #include <stdio.h>
 void print_alphabet(void);
+0x02-functions_nested_loops;
