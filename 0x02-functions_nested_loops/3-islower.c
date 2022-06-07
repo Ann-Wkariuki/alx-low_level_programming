4456930ae2693119ed5ff7b8c7d180680cce3e8c
ann developer <annwaiyegok@gmail.com>
2022-06-07 07:11:11 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,10 +8,17 @@
  * Return: 1 if int c is lowercase, 0 if otherwise 
  *
  */
-int _islower(int c)
+int _islower(int r)
 {
-	if (c >= 'a' && c <= 'z')
-		putchar(1);
+	if (r >= 'a' && r <= 'z')
+		return (1);
 	else
+		r = _islower('H');
+	 _putchar(r + '0');
+	 r = _islower('o');
+	 _putchar(r + '0');
+	 r = _islower(108);
+	  _putchar(r + '0');
+	  _putchar('\n');
 		return (0);
 }
