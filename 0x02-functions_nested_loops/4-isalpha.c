5a8f47dd8627f218b99a875f02270f1e57fbd4b4
ann developer <annwaiyegok@gmail.com>
2022-06-07 11:23:41 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * _isalpha - prints 1 if the value entered is an alphabet
@@ -11,7 +10,11 @@
 int _isalpha(int c);
 {
 	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
