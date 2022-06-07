7d6194c2d0a2729050f52660980665d5f18ae2e0
ann developer <annwaiyegok@gmail.com>
2022-06-07 02:03:04 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints a string
+ *
+ * Descripition: prints a string
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char a;
+	a = "_putcharn\n";
+	putchar a;
+	return (0);
+}
