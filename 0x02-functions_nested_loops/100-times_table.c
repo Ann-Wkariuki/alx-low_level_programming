ce1f2ba026120d30703d0f9a7a01c8300e149687
ann developer <annwaiyegok@gmail.com>
2022-06-07 20:20:07 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,16 +22,15 @@ void print_times_table(int n)
 
 				prod = num * mult;
 
-				if (prod <= 99)
-					putchar(' ');
-				if (prod <= 9)
-					putchar(' ');
+				if (prod <= 99; prod <= 9)
+				putchar(' ');
 
-				if (prod >= 100)
+				else if (prod >= 100)
 				{
 					putchar((prod / 100) + '0');
 					putchar(((prod / 10)) % 10 + '0');
 				}
+
 				else if (prod <= 99 && prod >= 10)
 				{
 					putchar((prod / 10) + '0');
