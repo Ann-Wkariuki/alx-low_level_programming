53fb2ab6b7f9b596885ee38e5ee210b41c9cd437
ann developer <annwaiyegok@gmail.com>
2022-06-09 04:37:39 -0700

    more loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_isupper - checks for uppercase alphabets
+ *@c: character to be checked
+ *return: returns 1 if c is uppercase and 0 otherwise
+ */
+
+int _isupper(int c)
+{
+
+	if(_isupper(c))
+{
+
+	return (0);
+}
+{
+	else
+}
+
+{
+	return (0);
+}
+}
