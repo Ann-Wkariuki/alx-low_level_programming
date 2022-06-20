8b67cc6bacafd2368ab35ceb6e26c334e11a66ca
ann developer <annwaiyegok@gmail.com>
2022-06-20 06:25:48 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ *_strspn - Locates a character in a string
+ *@s: This is the main C string to be scanned.
+ *@accept: This is the string containing the list of characters to match in s
+ *Return: return count
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int a,b;
+	int c = 0;
+	char *str1, *str2;
+
+	str1 = s;
+	str2 = accept;
+
+	a = 0;
+	while (str1[a] != '\0') /*Declaring WHILE *s */
+	{
+		b = 0;
+		while(str2[b] != '\0') /*Declaring WHILE *accept*/
+		{
+			if(str1[a] == str2[b])
+					{
+						c++;/*count value*/
+						break;
+					}
+			b++;/*add b+1*/
+		}
+		if (s[b] != accept[b]) /*If aren't equals*/
+		{
+			break;
+		}
+
+		a++;/*add x+1*/
+	}
+	return (c);
+
+}
