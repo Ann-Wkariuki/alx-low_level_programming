9a1b57efcca4bb6c55475eea78eaf85ab2efde0c
ann developer <annwaiyegok@gmail.com>
2022-06-20 02:17:04 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_memset - replaces a memory space with a character
+ *@b: the value to be copied in the memory
+ *@n: the number of times
+ *@s: sstring to be replaced
+ *Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	_memset(s, 'b', in);
+	return (*s);
+
+}
