3015ee14c96a3a54e9134f689e7167dffcbeb0c4
ann developer <annwaiyegok@gmail.com>
2022-06-27 07:35:35 -0700

    fixing 100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,56 +9,34 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int a, b;
+	int i, n, k = 0, len = 0;
+	char *str;
 
-	for (a = 0; dest[a] != '\0'; a++)
-		;
-	for (b = 0; src[b] != '\0'; b++)
-	{
-		dest[a] = src[b];
-		a++;
-	}
-	 return (dest);
-}
-/**
- *_strlen - Return the number of the length of the string
- *@s: This is the string to check
- *Return: Return the length of the string
- */
-int _strlen(char *s)
-{
-	int a;
-
-	for (a = 0; s[a] != '\0'; a++)
-		;
-	return (a);
-}
-/**
- *argstostr - Concats argv to new string
- * @ac: Argument count
- *@av: Arguments
- *Return: Returns a pointer to a new string, or NULL if it fails
- */
-char *argstostr(int ac, char **av)
-{
-	int index, sumString;
-	char *newString;
-
-	sumString = 1
-
-		if (ac == 0 || av == NULL)
-			return (0);
-	for (index = 0; index < ac && av[index]; index++)
-	{
-		sumString += _strlen(av[index]);
-	}
-	newString = malloc((sumString * sizeof(char)) + ac);
-	for (index = 0; index < ac && av[index]; index++)
-	{
-		newString = _strcat(newString, av[index]);
-		newString = _strcat(newString, "\n");
-	}
-	if (newString == NULL)
+	if (ac == 0 || av == NULL)
 		return (NULL);
-	return (newString);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+			len++;
+	}
+	len += ac;
+
+	str = malloc(sizeof(char) * len + 1);
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+		{
+			str[k] = av[i][n];
+			k++;
+		}
+		if (str[k] == '\0')
+		{
+			str[k++] = '\n';
+		}
+	}
+	return (str);
 }
