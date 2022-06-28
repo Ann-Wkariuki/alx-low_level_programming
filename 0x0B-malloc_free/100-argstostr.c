a1af4cc6102b01e1ccb2d3d8cded6fbf22d83644
ann developer <annwaiyegok@gmail.com>
2022-06-28 12:58:52 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -7,30 +7,30 @@
  *@src: Source string
  *Return: Concat newString to dest
  */
-char *_strcat(char *dest, char *src)
+char *argstostr(int ac, char **av)
 {
 	int i, n, k = 0, len = 0;
 	char *str;
 
-	if (dest == 0 || src == NULL)
+	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (i = 0; i < dest; i++)
+	for (i = 0; i < ac; i++)
 	{
-		for (n = 0; src[i][n]; n++)
+		for (n = 0; av[i][n]; n++)
 			len++;
 	}
-	len += dest;
+	len += ac;
 
 	str = malloc(sizeof(char) * len + 1);
 	if (str == NULL)
 		return (NULL);
 
-	for (i = 0; i < dest; i++)
+	for (i = 0; i < ac; i++)
 	{
-		for (n = 0; src[i][n]; n++)
+		for (n = 0; av[i][n]; n++)
 		{
-			str[k] = scr[i][n];
+			str[k] = av[i][n];
 			k++;
 		}
 		if (str[k] == '\0')
