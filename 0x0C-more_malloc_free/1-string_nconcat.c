def59e45da02c6734945397d507bfb4cd11caf0a
ann developer <annwaiyegok@gmail.com>
2022-06-30 10:40:41 -0700

    malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,39 +11,35 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *arr;
-	unsigned int i, j, co, co_2;
+	char *str;
+	unsigned int s1_len, s2_len, i;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
+	for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
+		;
+
+	for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
+		;
+
+	str = malloc(s1_len + s2_len + 1);
+
+	if (n >= s2_len)
+		n = s2_len;
+
 	for (i = 0; s1[i] != '\0'; i++)
-	{
-	}
+		str[i] = s1[i];
 
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-	}
+	for (i = 0; i < n; i++)
+		str[s1_len + i] = s2[i];
 
-	if (n < j)
-		j = n;
+	str[s1_len + n] = '\0';
 
-	j += i;
-	arr = malloc(sizeof(char *) * (j + 1));
-
-	if (arr == NULL)
+	if (str == NULL)
 		return (NULL);
 
-	for (co = 0; co < i; co++)
-		arr[co] = s1[co];
-	for (co_2 = 0; co < j; co_2++)
-	{
-		arr[co] = s2[co_2]
-			co++;
-	}
-	co++;
-	arr[co] = '\0';
-	return (arr);
+	return (str);
 }
