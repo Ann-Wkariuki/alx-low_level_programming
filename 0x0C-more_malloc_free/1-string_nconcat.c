57df4319fa976ddd115294f2b23d0ffb131f7be5
ann developer <annwaiyegok@gmail.com>
2022-06-30 11:53:44 -0700

    fixing 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,45 +1,45 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
+
 /**
- *string_nconcat - len of 1st str, len of 2nd str, if n < 2nd, 2nd = n
- *2nd + 1st = total len, malloc + null byte, loop to insert into temp arr
- * @s1: input one
- *@s2: input two
- *@n: s2's number of bytes
- *Return: 0
+ *string_nconcat - prints concatenate string;
+ * @s1: input string.
+ * @s2: input string.
+ *@n: len s2 string for print.
+ *Return: Nothing.
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str;
-	unsigned int s1_len, s2_len, i;
+	unsigned int l1, i, e;
+	char *a;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
+	l1 = 0;
+	while (s1[l1])
+		l1++;
 
-	for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
-		;
+	a = malloc(sizeof(*a) * l1 + n + 1);
 
-	for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
-		;
-
-	str = malloc(s1_len + s2_len + 1);
-
-	if (n >= s2_len)
-		n = s2_len;
-
-	for (i = 0; s1[i] != '\0'; i++)
-		str[i] = s1[i];
-
-	for (i = 0; i < n; i++)
-		str[s1_len + i] = s2[i];
-
-	str[s1_len + n] = '\0';
-
-	if (str == NULL)
+	if (a == NULL)
 		return (NULL);
 
-	return (str);
+	for (i = 0, e = 0; i < (l1 + n); i++)
+	{
+		if (i < l1)
+		{
+			a[i] = s1[i];
+		}
+		else
+		{
+			a[i] = s2[e++];
+		}
+	}
+	a[i] = '\0';
+	return (a);
 }
