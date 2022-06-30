d76744b728dc71cfd023972686d61fe81f89434d
ann developer <annwaiyegok@gmail.com>
2022-06-30 11:34:41 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- *malloc_checked - Entry Point
- *@b: input amount
- *Return: pointer to new mem
+ *malloc_checked - allocate memory.
+ *@b: size of the memory to be allocated.
+ *Return: pointer;
  */
-void *malloc_checked(unsigned int b);
+
+void *malloc_checked(unsigned int b)
 {
 	char *ip;
 
