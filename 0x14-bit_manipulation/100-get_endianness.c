46d588844f61b80954e26cf01ef6a2d33f30b767
ann <annwaiyegok@gmail.com>
2022-07-21 12:46:25 +0000

    2 to 100

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+include "main.h"
+/**
+ *get_bit - Gets the value of a bit at a given index.
+ *@n: The bit.
+ *@index: The index to get the value at - indices start at 0.
+ *
+ *Return: If an error occurs - -1.
+ *      Otherwise - The value of bit at index.
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *)&i;
+
+	return (*c);
+}
