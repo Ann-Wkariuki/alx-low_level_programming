f0a242ccc26422f28475e79c1aeb615352246e8f
ann developer <annwaiyegok@gmail.com>
2022-07-21 10:54:23 -0700

    betty fixed

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,11 +1,8 @@
 #include "main.h"
 /**
- *get_bit - Gets the value of a bit at a given index.
- *@n: The bit.
- *@index: The index to get the value at - indices start at 0.
- *
+ *get_endianness - Gets the password
  *Return: If an error occurs - -1.
- *      Otherwise - The value of bit at index.
+ *
  */
 int get_endianness(void)
 {
