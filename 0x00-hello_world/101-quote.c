a9dbe5f2c825ef0845ac3bf4b6a17788262f93c5
ann developer <annwaiyegok@gmail.com>
2022-07-24 03:46:30 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	char A[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19 \n";
+	char A[] = "and that piece of art is useful"" - Dora Korpar, 2015-10-19 \n";
 
 	fwrite(2, A, 59);
 	return (1);
